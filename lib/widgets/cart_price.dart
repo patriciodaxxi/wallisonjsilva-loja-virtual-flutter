336b3448005e4a42d5ac8ebf4f0584c0f0900bd3
Wallison Junior Cardoso Soares Silva <wallison.cardoso@hotmail.com>
2019-11-20 15:30:44 -0300

    primeiro commit

---
+++ lib/widgets/cart_price.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/cart_model.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class CardPrice extends StatelessWidget {
+  final VoidCallback buy;
+
+  CardPrice(this.buy);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        child: ScopedModelDescendant<CartModel>(
+          builder: (context, child, model) {
+            double price = model.getProductsPrice();
+            double discount = model.getDiscount();
+            double ship = model.getShipPrice();
+
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  "Resumo do pedido",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  height: 12.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Subtotal"),
+                    Text("R\$ ${price.toStringAsFixed(2)}"),
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Desconto"),
+                    Text("R\$ ${discount.toStringAsFixed(2)}"),
+                  ],
+                ),
+                Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Entrega"),
+                    Text("R\$ ${ship.toStringAsFixed(2)}"),
+                  ],
+                ),
+                Divider(),
+                SizedBox(
+                  height: 12.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Total",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "R\$ ${(price + ship - discount).toStringAsFixed(2)}",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12.0,
+                ),
+                RaisedButton(
+                  child: Text("Finalizar Pedido"),
+                  textColor: Colors.white,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: buy,
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
